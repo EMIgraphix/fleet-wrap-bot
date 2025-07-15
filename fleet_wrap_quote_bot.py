aefab3bec9e3b8e55653d721515b62bf26759a48
EMIgraphix <megangrubbs88@gmail.com>
2025-07-15 15:42:49 -0400

    Update fleet_wrap_quote_bot.py

--- fleet_wrap_quote_bot.py
+++ fleet_wrap_quote_bot.py
@@ -1,5 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from openai import OpenAI
 import os
+
+app = Flask(__name__)
+CORS(app)
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
